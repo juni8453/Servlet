d40fc0e14f933fb9591b26f61bdb7212b32fcd82
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-29 00:52:36 +0900

    Servlet 실행을 위한 어노테이션 작성 및 RequestHeader 정보 출력을 위한 properties 설정 추가

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 서블릿 자동 등록 (현재 패키지 하위 서블릿을 모두 자동으로 등록해준다.)
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
