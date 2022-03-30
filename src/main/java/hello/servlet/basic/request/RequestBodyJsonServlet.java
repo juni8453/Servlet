c1aac89cd95843348732fe2f24b023164e0a9ab9
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-30 12:41:35 +0900

    JSON 형식 데이터 전송 예제

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,2 +1,39 @@
-package hello.servlet.basic.request;public class RequestBodyJsonServlet {
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    // RequestBodyStringServlet 클래스에서 Body 의 단순 텍스트를 출력하는 것과 동일하다.
+    // 왜냐하면, HTTP 메시지 바디 데이터를 출력하는 것이 동일하기 떄문이다.
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        // JSON 을 객체로 파싱하기 위해서는 ObjectMapper 클래스의 도움이 필요하다.
+        // readValue() 메서드에 받은 messageBody, 파싱하고 싶은 객체를 인자로 넣어주면 된다.
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.getUsername = " + helloData.getUsername());
+        System.out.println("helloData = " + helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
 }
