b5affec10e57ce2f77b47b3144d85b445b6893f3
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-30 12:41:14 +0900

    HTML Form 형식 데이터 전송을 위한 폼 생성

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -1,2 +1,28 @@
-package hello.servlet.basic.request;public class RequestBodyStringServlet {
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // messageBody 내용을 inputStream 을 통해 바이트 코드로 뽑아낸다.
+        // StreamUtils.copyToString() 을 통해 뽑아낸 바이트 코드를 String 으로 변환할 수 있다.
+        // 바이트 코드를 문자열로 변환할 때는 항상 어떤 인코딩을 사용할 것인지 명시해야 한다.
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
 }
