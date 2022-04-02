4a922ad498ae7629a453a460da075d5799116b31
ByeoungJoon Jeon <juni8453@naver.com>
2022-04-02 15:01:23 +0900

    서블릿, JSP 연습을 위한 Singleton 형식의 간단한 Repository 추가

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -1,2 +1,42 @@
-package hello.servlet.domain.member;public class MemberRepository {
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    // 강의에서는 static 키워드가 있었는데, Singleton 으로 MemberRepository 클래스를
+    // 사용하기 때문에 굳이 필요는 없어서 해당 예제에서는 제외했다.
+
+    private final Map<Long, Member> store = new HashMap<>();
+    private Long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    private MemberRepository() {}
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    // ArrayList 의 값을 조작하거나 바꿔도 store 에 영향을 주지 않기 위한 구조
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,4 +1,58 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
 class MemberRepositoryTest {
-  
+
+    MemberRepository repository;
+
+    @BeforeEach
+    void beforeEach() {
+        repository = MemberRepository.getInstance();
+    }
+
+    @AfterEach
+    void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    @DisplayName("Member 를 저장하고 store 와 비교했을 때 저장된 멤버와 찾아온 멤버가 같아야 한다.")
+    void save() {
+        Member test1 = new Member("test1", 20);
+        Member saveMember = repository.save(test1);
+        Member findMember = repository.findById(saveMember.getId());
+
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    @DisplayName("Member 를 저장하고 store 에 저장된 멤버 Id 와 저장한 멤버 Id 가 같아야 한다.")
+    void findById() {
+        Member test1 = new Member("test1", 20);
+        Member saveMember = repository.save(test1);
+
+        assertThat(saveMember.getId()).isEqualTo(test1.getId());
+    }
+
+    @Test
+    @DisplayName("여러 멤버를 저장하면서 사이즈를 비교했을 때 저장한 멤버만큼의 사이즈가 나오고, 각 인덱스 별 username 이 같아야 한다. ")
+    void findAll() {
+        Member test1 = new Member("test1", 20);
+        Member test2 = new Member("test2", 30);
+
+        Member saveMember1 = repository.save(test1);
+        Member saveMember2 = repository.save(test2);
+
+        List<Member> allMember = repository.findAll();
+
+        assertThat(allMember.size()).isEqualTo(2);
+        assertThat(allMember.get(0).getUsername()).isEqualTo(saveMember1.getUsername());
+        assertThat(allMember.get(1).getUsername()).isEqualTo(saveMember2.getUsername());
+    }
 }
